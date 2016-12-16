272546e77e44c82fd5e43b5ed83fa6cab827c31c
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-12-16 18:24:47 +0530

    found the combinations random generated string

--- random_string.rb
+++ random_string.rb
@@ -1,4 +1,8 @@
 puts "Enter a number"
 n = gets.chomp.to_i
 string = (0...n).map { ('a'..'z').to_a[rand(n)] }
-puts string
+arr=Array.new
+(0..n).flat_map{|n| string.to_a.combination(n).map(&:join)}.each do |c|
+	arr.push(c)			
+end
+puts arr
