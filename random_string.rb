bc7b9963b391e18c1bc8fb42a6ed39d3c0695cd0
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-12-16 18:28:08 +0530

    find permutations of all combination

--- random_string.rb
+++ random_string.rb
@@ -3,6 +3,6 @@ n = gets.chomp.to_i
 string = (0...n).map { ('a'..'z').to_a[rand(n)] }
 arr=Array.new
 (0..n).flat_map{|n| string.to_a.combination(n).map(&:join)}.each do |c|
-	arr.push(c)			
+	arr.push(c.chars.to_a.permutation.map &:join)			
 end
 puts arr
