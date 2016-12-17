a23cb496ad6b31ed231285028768d7fa8d774055
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-12-17 10:29:46 +0530

    found english words from permutation

--- random_string.rb
+++ random_string.rb
@@ -6,3 +6,14 @@ arr=Array.new
 	arr.push(c.chars.to_a.permutation.map &:join)			
 end
 puts arr
+words = {}
+File.open("/usr/share/dict/words") do |file|
+  file.each do |line|
+     words[line.strip] = true
+  end 
+end
+arr.each do |a|
+  	if words[a.to_s.strip] == true
+		puts a
+        end
+end
